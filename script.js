4805876e5c0b115d3af65b7d634dbd1d852ba268
Mustafatshareef <mustafa.99sharif@gmail.com>
2026-07-09 19:17:52 +0300

    add input vaidation to todo ap

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-function changetext() {
-    document.getElementById("tittle").innerHTML = "Hello World";
-}
+document.getElementById("addBtn").addEventListener("click", function() {
+
+    if (document.getElementById("taskInput").value === "") {
+        alert("Please enter a task!");
+        return;
+    }
+    console.log(document.getElementById("taskInput").value);
+    let task = document.getElementById("taskInput").value;
+    let li = document.createElement("li");
+    li.innerHTML = task;
+    document.getElementById("taskList").appendChild(li);
+    document.getElementById("taskInput").value = "";
+});
